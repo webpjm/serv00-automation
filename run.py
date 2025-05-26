6dfd33dea312b8fc9bdd5d9113b5b9d3c033a396
webpjm <13012264890@163.com>
2025-05-26 11:02:50 +0800

    Update run.py

--- run.py
+++ run.py
@@ -39,46 +39,4 @@ menu = requests.get('https://api.zzzwb.com/v1?get=tg').json()
 loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
 content += f"本次登录用户共： {user_num} 个\n登录时间：{time}\n登录IP：{loginip}"
 
-push = os.getenv('PUSH')
-
-def mail_push(url):
-    data = {
-        "body": content,
-        "email": os.getenv('MAIL')
-    }
-
-    response = requests.post(url, json=data)
-
-    try:
-        response_data = json.loads(response.text)
-        if response_data['code'] == 200:
-            print("推送成功")
-        else:
-            print(f"推送失败，错误代码：{response_data['code']}")
-    except json.JSONDecodeError:
-        print("连接邮箱服务器失败了")
-
-def telegram_push(message):
-    url = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_BOT_TOKEN')}/sendMessage"
-    payload = {
-        'chat_id': os.getenv('TELEGRAM_CHAT_ID'),
-        'text': message,
-        'parse_mode': 'HTML',
-        'reply_markup': json.dumps({
-            "inline_keyboard": menu,
-            "one_time_keyboard": True
-         })
-    }
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code != 200:
-        print(f"发送消息到Telegram失败: {response.text}")
-
-if push == "mail":
-    mail_push('https://zzzwb.pp.ua/test')
-elif push == "telegram":
-    telegram_push(content)
-else:
-    print("推送失败，推送参数设置错误")
+print(content)
